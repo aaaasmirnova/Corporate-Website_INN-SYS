551cf8804b23b81ead604dc49170623151d93074
aaaasmirnova <smirnovnana@yandex.ru>
2026-03-06 08:01:34 +0300

    feat: integrate Tailwind with Storybook, add Footer/Header stories

---
+++ stories/Footer.stories.ts
@@ -0,0 +1,18 @@
+import { Footer } from "../components/Footer";
+
+const meta = {
+  title: "Layout/Footer",
+  component: Footer,
+};
+
+export default meta;
+
+export const Default = {};
+
+export const Mobile = {
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+  },
+};
