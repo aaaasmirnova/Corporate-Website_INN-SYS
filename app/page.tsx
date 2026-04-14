e442cc43d03fea0c2fd48fc322d49314c4a4ef2e
aaaasmirnova <smirnovnana@yandex.ru>
2026-04-14 16:39:17 +0300

    feat: add page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Footer } from "@/components/Footer";
+import { Career } from "@/components/Career/Career";
+import { MainLayout } from "@/components/MainLayout/MainLayout";
+import { InvestorSection } from "@/components/InvestorSection/InvestorSection";
 
 export default function Home() {
   return (
-    <div>
-      <Footer />
-    </div>
+    <MainLayout>
+      <InvestorSection />
+      <Career />
+      {/* <Footer /> */}
+    </MainLayout>
   );
 }
