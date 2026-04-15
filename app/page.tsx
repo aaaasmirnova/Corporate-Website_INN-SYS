bfafc9db9c0f80c3760a7931566b71db879156b5
Mariya Shusharina <mariya.shushar@gmail.com>
2026-04-15 12:32:45 +0300

    feat: add AboutSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { HeroSection } from "@/components/HeroSection";
+import { AboutSection } from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Header />
       <HeroSection />
+      <AboutSection />
       <Footer />
     </div>
   );
