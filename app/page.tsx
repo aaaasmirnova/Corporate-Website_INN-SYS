0e639e00564519282864604a2bde2832c997a364
Mariya Shusharina <mariya.shushar@gmail.com>
2026-04-15 12:32:02 +0300

    feat: add HeroSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <Header />
+      <HeroSection />
       <Footer />
     </div>
   );
---
+++ components/HeroSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import bgImage from "../public/bg-hero.png";
+
+export const HeroSection = () => {
+  return (
+    <section
+    id="hero"
+      className="w-full flex flex-col items-center py-[232px] bg-[url(../public/bg-hero.png)] bg-hero bg-no-repeat bg-center bg-cover"
+      style={{ backgroundImage: `url(${bgImage})`}}
+    >
+      <h1 className="font-montserrat font-semibold text-white text-[64px]">Инноватика Systems</h1>
+      <p className="max-w-[643px] my-[64px] text-white text-center font-montserrat text-[24px]">Проектируем, внедряем и сопровождаем комплексные ИТ-решения для государственных и корпоративных организаций. Наша задача — сделать данные понятными, процессы — управляемыми, результаты — предсказуемыми</p>
+      <button className="my-[64px] py-[12px] px-[32px] bg-white rounded-xl font-[16px] font-openSans">Обсудить проект</button>
+    </section>
+  );
+};
