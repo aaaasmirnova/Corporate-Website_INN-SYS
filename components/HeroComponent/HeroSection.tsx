5a5f2e3f6f32337f3b44929264fc0706a8822818
Mariya Shusharina <mariya.shushar@gmail.com>
2026-04-18 15:40:46 +0300

    refactor: add cursor-pointer to button in HeroSection

--- components/HeroComponent/HeroSection.tsx
+++ components/HeroComponent/HeroSection.tsx
@@ -6,11 +6,11 @@ export const HeroSection = () => {
     <section
       id="hero"
       className="w-full flex flex-col items-center py-[232px] bg-[url(../public/assets/png/bg-hero.png)] bg-hero bg-no-repeat bg-center bg-cover"
-      style={{ backgroundImage: `url(${bgImage})`}}
+      style={{ backgroundImage: `url(${bgImage})` }}
     >
       <h1 className="font-heading font-semibold text-white text-[64px]">Инноватика Systems</h1>
       <p className="max-w-[643px] my-[64px] text-white text-center font-paragraph text-[18px]">Проектируем, внедряем и сопровождаем комплексные ИТ-решения для государственных и корпоративных организаций. Наша задача — сделать данные понятными, процессы — управляемыми, результаты — предсказуемыми</p>
-      <button className="my-[64px] py-[12px] px-[32px] bg-white rounded-xl font-[16px] font-open-sans">Обсудить проект</button>
+      <button className="cursor-pointer my-[64px] py-[12px] px-[32px] bg-white rounded-xl font-[16px] font-open-sans">Обсудить проект</button>
     </section>
   );
 };
