4721b3d705a8e9b080ab29fe68d20cf61bb4a590
aaaasmirnova <smirnovnana@yandex.ru>
2026-04-20 11:01:55 +0300

    refactor: enhance product card with adaptive spacing and hover effect

--- components/ProductEcosystem/ProductEcosystem.tsx
+++ components/ProductEcosystem/ProductEcosystem.tsx
@@ -5,7 +5,7 @@ export const ProductEcosystem = () => {
   const [mainProduct, ...otherProducts] = products;
 
   return (
-    <section className="flex flex-col text-white bg-neutral-black-elbrus w-full mb-50">
+    <section className="flex flex-col text-white bg-neutral-black-elbrus w-full mb-[107px]">
       <div className="text-center w-full">
         <h3 className="text-[24px] md:text-[32px]  lg:text-[40px]  leading-[150%] text-center font-heading font-semibold mb-12 md:mb-20 lg:mb-24 gradient-text inline-block">
           Продуктовая экосистема
@@ -13,19 +13,15 @@ export const ProductEcosystem = () => {
       </div>
 
       <div className="mx-auto">
-        {/* Десктоп (≥1024px): первый продукт на всю ширину */}
-
-        {/* Десктоп (≥1024px): первый продукт на всю ширину */}
         <div className="hidden lg:block mb-6 group">
-          <div className="bg-surface-1 hover:bg-surface-2 active:bg-surface-3 p-8 rounded-[24px] overflow-hidden relative">
+          <div className="bg-surface-1 hover:bg-surface-2 active:bg-surface-3 p-8 rounded-[24px] overflow-hidden relative lg:h-[272px]">
             <h3 className="text-[24px] lg:text-[28px] leading-[150%] font-semibold font-heading mb-28">
               {mainProduct.name}
             </h3>
-            <p className="text-[16px] lg:text-[18px] leading-[150%] font-medium font-heading whitespace-pre-line">
+            <p className="text-[14px] md:text-[16px] lg:text-[18px] leading-[150%] font-medium font-heading whitespace-pre-line  lg:max-w-[300px]">
               {mainProduct.description}
             </p>
 
-            {/* Все звёзды из массива */}
             {mainProduct.images?.map((img, idx) => (
               <div
                 key={idx}
@@ -42,26 +38,30 @@ export const ProductEcosystem = () => {
           </div>
         </div>
 
-        {/* Сетка: мобилка 1 колонка, планшет 2 колонки, десктоп 3 колонки */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {/* Первый продукт в сетке для мобилки/планшета */}
-
           <div className="block lg:hidden group">
-            <div className="bg-surface-1 hover:bg-surface-2 active:bg-surface-3 p-8 rounded-[24px] relative">
-              <div className="absolute top-4 right-4 overflow-hidden rounded-lg">
-                {/* <Image
-                  src={mainProduct.image}
-                  alt={mainProduct.name}
-                  fill
-                  className={`${mainProduct.imageClassName} object-cover transition-transform duration-500 group-hover:scale-[1.094]`}
-                /> */}
+            <div className="bg-surface-1 hover:bg-surface-2 active:bg-surface-3 p-6 rounded-[24px] relative overflow-hidden md:h-[216px] lg:h-[272px] flex flex-col justify-between">
+              {mainProduct.images?.map((img, idx) => (
+                <div
+                  key={idx}
+                  className={`${img.className} absolute overflow-hidden rounded-lg pointer-events-none`}
+                >
+                  <Image
+                    src={mainProduct.image}
+                    alt={mainProduct.name}
+                    fill
+                    className="object-cover transition-transform duration-500 group-hover:scale-[1.094]"
+                  />
+                </div>
+              ))}
+              <div className="relative z-10">
+                <h3 className="text-[18px] md:text-[24px] lg:text-[28px] leading-[150%] font-semibold font-heading mb-21">
+                  {mainProduct.name}
+                </h3>
+                <p className="text-[14px] md:text-[16px] lg:text-[18px] leading-[150%] font-medium font-heading whitespace-pre-line max-w-[300px] md:max-w-[252px]">
+                  {mainProduct.description}
+                </p>
               </div>
-              <h3 className="text-[24px] lg:text-[28px] leading-[150%] font-semibold font-heading mb-21">
-                {mainProduct.name}
-              </h3>
-              <p className="text-[16px] lg:text-[18px] leading-[150%] font-medium font-heading whitespace-pre-line">
-                {mainProduct.description}
-              </p>
             </div>
           </div>
 
@@ -71,10 +71,9 @@ export const ProductEcosystem = () => {
 
             return (
               <div key={index} className="group">
-                <div className="bg-surface-1 hover:bg-surface-2 active:bg-surface-3 p-8 rounded-[24px] relative overflow-hidden">
+                <div className="bg-surface-1 hover:bg-surface-2 active:bg-surface-3 p-6 lg:py-8 lg:px-12 rounded-[24px] relative overflow-hidden md:h-[216px] lg:h-[272px] flex flex-col justify-between">
                   <div
                     className={`${product.imageClassName} absolute overflow-hidden rounded-lg transition-all duration-500`}
-                    style={{ zIndex: 1 }}
                   >
                     <Image
                       src={product.image}
@@ -83,15 +82,28 @@ export const ProductEcosystem = () => {
                       className="object-cover transition-transform duration-500 group-hover:scale-[1.094]"
                     />
                   </div>
-                  <div className="relative" style={{ zIndex: 2 }}>
-                    <h3
-                      className={`text-[24px] lg:text-[28px] leading-[150%] font-semibold font-heading  ${
-                        isElbrusCortes ? "mb-[58px]" : "mb-[85px]"
-                      }`}
-                    >
-                      {product.name}
-                    </h3>
-                    <p className="text-[16px] lg:text-[18px] leading-[150%] font-medium font-heading whitespace-pre-line">
+                  <div className="relative flex flex-col h-full">
+                    <div className="min-h-15">
+                      <div
+                        className={
+                          isElbrusCortes
+                            ? "mb-[30px] md:mb-[26px] lg:mb-[65px]"
+                            : "mb-[51px] md:mb-[48px] lg:mb-[75px]"
+                        }
+                      >
+                        <h3 className="text-[18px] md:text-[24px]  lg:text-[28px] leading-[150%] font-semibold font-heading">
+                          {isElbrusCortes ? (
+                            <div className="group-hover:flex group-hover:flex-col">
+                              <span>Elbrus </span>
+                              <span>Cortex</span>
+                            </div>
+                          ) : (
+                            product.name
+                          )}
+                        </h3>
+                      </div>
+                    </div>
+                    <p className="text-[14px] md:text-[16px] lg:text-[18px] leading-[150%] font-medium font-heading whitespace-pre-line mt-auto">
                       {product.description}
                     </p>
                   </div>
