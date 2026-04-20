416d7621456fef30bd173280dfa4b297ffa825df
aaaasmirnova <smirnovnana@yandex.ru>
2026-04-20 11:02:49 +0300

    feat:add Partners to page

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import { MainLayout } from "@/components/MainLayout/MainLayout";
 import { InvestorSection } from "@/components/InvestorSection/InvestorSection";
 import { Contacts } from "@/components/Contacts/Contacts";
 import { ProductEcosystem } from "@/components/ProductEcosystem/ProductEcosystem";
+import { Partners } from "@/components/Partners/Partners";
 
 export default function Home() {
   return (
     <MainLayout>
       <ProductEcosystem />
+      <Partners />
       <InvestorSection />
       <Career />
       <Contacts />
