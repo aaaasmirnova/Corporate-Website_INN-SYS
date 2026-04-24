9ada0d38be2e11a4211299e9dd7337addd1e63cb
Suren Simonyan <s.simonyan@combotech.net>
2026-04-24 22:45:43 +0400

    fix: form changes

--- app/components/ContactsForm/ContactsForm.tsx
+++ app/components/ContactsForm/ContactsForm.tsx
@@ -19,7 +19,7 @@ export default function ContactsForm() {
     register,
     handleSubmit,
     setValue,
-    formState: { errors, isSubmitting },
+    formState: { isValid, errors, isSubmitting },
     reset,
   } = useForm<FormValues>();
 
@@ -27,7 +27,9 @@ export default function ContactsForm() {
 
   const validateFile = (files: FileList) => {
     const file = files?.[0];
-    if (!file) return "File is required";
+
+    // 👇 THIS makes it optional
+    if (!file) return true;
 
     const allowedTypes = [
       "application/pdf",
@@ -91,7 +93,7 @@ export default function ContactsForm() {
 
   return (
     <section className="bg-neutral-black-elbrus">
-      <div className="max-w-214 w-full mx-auto px-5 text-white">
+      <div className="max-w-216 w-full mx-auto px-6 text-white">
         <h2 className="mb-12 bg-title-gradient bg-clip-text text-2xl font-semibold text-transparent text-center md:text-[40px] md:mb-24">
           Ответим на ваши вопросы
         </h2>
@@ -101,16 +103,20 @@ export default function ContactsForm() {
               <input
                 type="text"
                 placeholder="Имя"
-                className="w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6 "
+                className={`w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6 ${errors.name && "border-red-700"}`}
                 {...register("name", { required: "Name is required" })}
               />
-              {errors.name && <p>{errors.name.message}</p>}
+              {errors.name && (
+                <p className="text-red-700 text-xs px-4">
+                  {errors.name.message}
+                </p>
+              )}
             </div>
 
             <div className="w-full md:w-[calc(50%-12px)]">
               <input
                 type="text"
-                className="w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6"
+                className={`w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6  ${errors.name && "border-red-700"}`}
                 placeholder="Организация"
                 {...register("organization")}
               />
@@ -119,19 +125,23 @@ export default function ContactsForm() {
             <div className="w-full md:w-[calc(50%-12px)]">
               <input
                 type="tel"
-                className="w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6"
+                className={`w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6  ${errors.name && "border-red-700"}`}
                 placeholder="Телефон"
                 {...register("phone", {
                   required: "Phone is required",
                 })}
               />
-              {errors.phone && <p>{errors.phone.message}</p>}
+              {errors.phone && (
+                <p className="text-red-700 text-xs px-4">
+                  {errors.phone.message}
+                </p>
+              )}
             </div>
 
             <div className="w-full md:w-[calc(50%-12px)]">
               <input
                 type="email"
-                className="w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6"
+                className={`w-full h-12 px-4 border border-accent-5 rounded-xl focus:border-white placeholder:text-accent-6  ${errors.name && "border-red-700"}`}
                 placeholder="Email"
                 {...register("email", {
                   required: "Email is required",
@@ -141,7 +151,11 @@ export default function ContactsForm() {
                   },
                 })}
               />
-              {errors.email && <p>{errors.email.message}</p>}
+              {errors.email && (
+                <p className="text-red-700 text-xs px-4">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
           </div>
 
@@ -152,64 +166,83 @@ export default function ContactsForm() {
               className="w-full h-24 mb-6 py-2 px-4 border border-accent-5 rounded-xl resize-none focus:border-white placeholder:text-accent-6"
               {...register("message")}
             />
-            {errors.message && <p>{errors.message.message}</p>}
+            {errors.message && (
+              <p className="text-red-700 text-xs px-4">
+                {errors.message.message}
+              </p>
+            )}
           </div>
 
           {/* File Upload */}
           <div>
-            <label className="flex flex-col items-center justify-center h-24 mb-6 px-4 border border-dashed border-accent-5 rounded-xl cursor-pointer">
-              <p className="text-accent-6">
-                {selectedFile ? (
-                  <p className="mt-2 text-sm text-accent-6">
-                    <span className="text-primary-4">{selectedFile.name}</span>
-                  </p>
-                ) : (
-                  <>
-                    <span className="hidden md:inline">Перенесите файл сюда или</span>{" "}
-                    <span className="text-primary-4">прикрепите его</span>
-                  </>
-                )}
-              </p>
-              <input
-                type="file"
-                accept=".pdf,.doc,.docx,.xls,.xlsx"
-                className="hidden"
-                {...register("file", { validate: validateFile })}
-                onChange={handleFileChange}
-              />
-            </label>
-            {errors.file && <p>{errors.file.message as string}</p>}
+            <div className="mb-6">
+              <label className="flex flex-col items-center justify-center h-24 px-4 border border-dashed border-accent-5 rounded-xl cursor-pointer">
+                <p className="text-accent-6">
+                  {selectedFile ? (
+                    <p className="mt-2 text-sm text-accent-6">
+                      <span className="text-primary-4">
+                        {selectedFile.name}
+                      </span>
+                    </p>
+                  ) : (
+                    <>
+                      <span className="hidden md:inline">
+                        Перенесите файл сюда или
+                      </span>{" "}
+                      <span className="text-primary-4">прикрепите его</span>
+                    </>
+                  )}
+                </p>
+                <input
+                  type="file"
+                  accept=".pdf,.doc,.docx,.xls,.xlsx"
+                  className="hidden"
+                  {...register("file", { validate: validateFile })}
+                  onChange={handleFileChange}
+                />
+              </label>
+            </div>
           </div>
 
           {/* Privacy Policy */}
           <div className="mb-20 md:mb-28">
-            <label className="flex items-center gap-3 mb-5 md:mb-12">
-              <div className="flex cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="w-0 h-0 peer opacity-0"
-                  {...register("privacy", {
-                    required: "You must accept the privacy policy",
-                  })}
-                />
-                <span className="flex justify-center items-center w-4 h-4 border border-accent-7 rounded-sm before:content-[''] before:block before:h-3 before:w-2 before:border-primary-4 before:border-0 before:border-r-2 before:border-b-2 before:rotate-45 before:opacity-0 peer-checked:before:opacity-100"></span>
-              </div>
-              <p className="text-xs">
-                Я даю ООО «Инноватика Systems» согласие на обработку
-                персональных данных в целях получения консультаций в
-                соответствии с{" "}
-                <Link href="#privacy-policy" className="text-primary-4">
-                  политикой конфиденциальности
-                </Link>
-              </p>
-            </label>
-            {errors.privacy && <p>{errors.privacy.message}</p>}
+            <div className="mb-5 md:mb-12">
+              <label className="flex items-center gap-3">
+                <div className="flex cursor-pointer">
+                  <input
+                    type="checkbox"
+                    className="w-0 h-0 peer opacity-0"
+                    {...register("privacy", {
+                      required: "You must accept the privacy policy",
+                    })}
+                  />
+                  <span className="flex justify-center items-center w-4 h-4 border border-accent-7 rounded-sm before:content-[''] before:block before:h-3 before:w-2 before:border-primary-4 before:border-0 before:border-r-2 before:border-b-2 before:rotate-45 before:opacity-0 peer-checked:before:opacity-100"></span>
+                </div>
+                <p className="text-xs">
+                  Я даю ООО «Инноватика Systems» согласие на обработку
+                  персональных данных в целях получения консультаций в
+                  соответствии с{" "}
+                  <Link href="#privacy-policy" className="text-primary-4">
+                    политикой конфиденциальности
+                  </Link>
+                </p>
+              </label>
+              {errors.privacy && (
+                <p className="text-red-700 text-xs px-4">
+                  {errors.privacy.message}
+                </p>
+              )}
+            </div>
 
             {/* Submit */}
             <button
               type="submit"
-              className="block h-12 mx-auto px-8 bg-button-primary text-neutral-black-elbrus rounded-xl cursor-pointer md:min-w-64"
-              disabled={isSubmitting}
+              className={`block h-12 mx-auto px-8 bg-button-primary text-neutral-black-elbrus rounded-xl md:min-w-64 ${
+                isSubmitting || !isValid
+                  ? "text-button-inactive cursor-not-allowed"
+                  : "cursor-pointer"
+              }`}
+              disabled={isSubmitting || !isValid}
             >
               {isSubmitting ? "Submitting..." : "Submit"}
             </button>
