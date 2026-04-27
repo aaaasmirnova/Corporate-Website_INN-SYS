4ffce02566a0bc017ee7a9f0453749b8128d26de
Mariya Shusharina <mariya.shushar@gmail.com>
2026-04-27 20:09:12 +0300

    fix: correct styles in KeyIndicators Section and add adaptiveness

--- components/KeyIndicatorsComponent/KeyCard.tsx
+++ components/KeyIndicatorsComponent/KeyCard.tsx
@@ -4,12 +4,12 @@ interface IKeyCardProps {
 }
 
 export const KeyCard = ({ info }: IKeyCardProps) => {
-  const cardClass: string = "p-6 max-w-[400px] flex flex-col gap-2 bg-neutral-bright-beginning-4 backdrop-blur-xs rounded-3xl";
+  const cardClass: string = "p-6 max-w-[400px] flex flex-col gap-2 bg-neutral-bright-beginning-4 backdrop-blur-xs rounded-3xl " +
+  `card-${info.id}`;
 
   return (
     <article
       className={cardClass}
-      style={{ transform: `translate(${info.desktop.translate.x}, ${info.desktop.translate.y})` }}
     >
       <h3 className="font-heading font-semibold text-h1-desktop text-neutral-bright-beginning">{info.title}</h3>
       <p className="text-body-desktop text-neutral-bright-beginning">{info.description}</p>
--- components/KeyIndicatorsComponent/KeyIndicatorsSection.tsx
+++ components/KeyIndicatorsComponent/KeyIndicatorsSection.tsx
@@ -3,15 +3,13 @@ import { cards } from "./data";
 import { KeyCard } from "./KeyCard";
 
 export const KeyIndicatorsSection = () => {
-  const divClass = "my-16 grid grid-cols-2 grid-rows-2 gap-50 bg-[url(../public/assets/png/key-picture.png)] bg-hero bg-no-repeat bg-center";
-
   return (
     <section
       id="key-indicators"
-      className="w-full flex flex-col gap-12 items-center pt-16 pb-36"
+      className="w-full flex flex-col gap-12 items-center pt-16 pb-36 bg-[url(../public/assets/png/key-picture.png)] bg-no-repeat bg-center"
     >
       <h1 className="font-heading font-semibold bg-radial from-neutral-bright-beginning to-neutral-grey-300 bg-clip-text text-transparent text-[40px]">Ключевые показатели</h1>
-      <div className={divClass}>
+      <div className="my-16 grid grid-cols-1 grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-8 md:gap-5 lg:gap-24 xl:gap-50">
         {cards.map((card, index) => (
           <KeyCard info={card} key={index} />
         ))}
