954d130292fa9a670cc138989cb2e554fed74ec2
Mariya Shusharina <mariya.shushar@gmail.com>
2026-04-27 22:29:00 +0300

    fix: add gap in text of Hero Section at mobile width

--- components/HeroComponent/HeroSection.tsx
+++ components/HeroComponent/HeroSection.tsx
@@ -7,7 +7,12 @@ export const HeroSection = () => {
       className="w-full h-lvh flex flex-col items-center py-58 bg-[url(../public/assets/png/bg-hero.png)] bg-hero bg-no-repeat bg-center bg-cover"
     >
       <h1 className="font-heading font-semibold text-white text-[24px] md:text-[32px] xl:text-[64px]">Инноватика Systems</h1>
-      <p className="max-w-81.75 md:max-w-130.5 xl:max-w-160.75 my-16 text-white text-center font-paragraph text-[14px] md:text-[18px] xl:text-[18px]">Проектируем, внедряем и сопровождаем комплексные ИТ-решения для государственных и корпоративных организаций. Наша задача — сделать данные понятными, процессы — управляемыми, результаты — предсказуемыми</p>
+      <p className="max-w-81.75 md:max-w-130.5 xl:max-w-160.75 my-16 text-white text-center font-paragraph text-[14px] md:text-[18px] xl:text-[18px]">
+        Проектируем, внедряем и сопровождаем комплексные ИТ-решения для государственных и корпоративных организаций. 
+        <span className="block mt-6 sm:inline sm:mt-0">
+          Наша задача — сделать данные понятными, процессы — управляемыми, результаты — предсказуемыми
+        </span>
+      </p>
       <button className="cursor-pointer my-16 py-3 px-8 bg-white rounded-xl font-[16px] font-open-sans">Обсудить проект</button>
     </section>
   );
