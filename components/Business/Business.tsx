d426351b1f12c411b2b3a1352298bcb72f41eb54
Suren Simonyan <s.simonyan@combotech.net>
2026-04-28 22:38:43 +0400

    fix: margin fix

--- components/Business/Business.tsx
+++ components/Business/Business.tsx
@@ -5,7 +5,7 @@ export const Business = () => {
   const t = useTranslations("Business");
 
   return (
-    <section className="relative flex flex-col max-w-214.5 w-full mx-auto">
+    <section className="relative flex flex-col max-w-214.5 w-full mx-auto mb-9 md:mb-18.75 xl:mb-24">
       <p className="absolute top-[15%] text-2xl font-semibold text-[#807E7E] text-center md:top-[25%] md:text-[32px] xl:text-[40px]">
         {t("description")}
       </p>
