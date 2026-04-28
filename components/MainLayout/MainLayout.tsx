35e0c36e3ac4974998cb2c6c5c029d8d254cb001
aaaasmirnova <smirnovnana@yandex.ru>
2026-04-28 21:29:06 +0300

    fix: remove max-width restriction from MainLayout

--- components/MainLayout/MainLayout.tsx
+++ components/MainLayout/MainLayout.tsx
@@ -7,9 +7,7 @@ interface MainLayoutProps {
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="min-h-screen bg-neutral-black-elbrus">
-      <div className="max-w-[1440px] mx-auto w-full px-6 md:px-8 xl:px-24">
-        {children}
-      </div>
+      <div className="px-6 md:px-8 xl:px-24">{children}</div>
     </div>
   );
 };
