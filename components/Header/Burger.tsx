ebfe6079f6e19dce76522f227dbeddd6e0c7824d
Mariya Shusharina <mariya.shushar@gmail.com>
2026-04-29 22:54:23 +0300

    fix: change Burger Icon to three 1px-width spans

--- components/Header/Burger.tsx
+++ components/Header/Burger.tsx
@@ -1,17 +1,13 @@
 import React from "react";
-import Image from "next/image";
-import BurgerImage from "../../public/assets/svg/burger.svg"
 
 export const Burger = () => {
   return (
-    <div className="cursor-pointer block xl:hidden p-4 rounded-xl bg-neutral-black-elbrus/20 backdrop-blur-xs">
-      <Image
-        className="my-1 mx-1.5"
-        src={BurgerImage}
-        alt="="
-        width="16"
-        height="12"
-      />
+    <div className="cursor-pointer flex justify-center items-center w-14 h-14 xl:hidden py-4 rounded-xl bg-neutral-black-elbrus/20 backdrop-blur-xs">
+      <div id="burger-div" className="flex flex-col gap-[4.5px] my-1 mx-1.5">
+        <span id="burger-line-top" className="block w-4 h-px bg-neutral-bright-beginning"></span>
+        <span id="burger-line-middle" className="block w-4 h-px bg-neutral-bright-beginning"></span>
+        <span id="burger-line-bottom" className="block w-4 h-px bg-neutral-bright-beginning"></span>
+      </div>
     </div>
   );
 };
