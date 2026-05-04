2da180034650cd4902ffdda549a3cccf44cd5d0a
Mariya Shusharina <mariya.shushar@gmail.com>
2026-05-04 17:40:45 +0300

    fix: update imports of svg assets

--- components/Header/Burger.tsx
+++ components/Header/Burger.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import Logo from "../../public/assets/svg/innovatica-logo.svg"
+import Logo from "@/public/assets/svg/innovatica-logo.svg"
 
 export const Burger = () => {
   const [isOpen, setOpen] = useState(false);
