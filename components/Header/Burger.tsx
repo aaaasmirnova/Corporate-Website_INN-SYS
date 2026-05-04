b166822776ffc43590212728cf3f987e28190f03
Mariya Shusharina <mariya.shushar@gmail.com>
2026-05-04 17:34:18 +0300

    feat: add navigation and functionality to burger-menu

--- components/Header/Burger.tsx
+++ components/Header/Burger.tsx
@@ -1,13 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+// import { BurgerNav } from "./BurgerNav";
+import Image from "next/image";
+import Logo from "../../public/assets/svg/innovatica-logo.svg"
 
 export const Burger = () => {
+  // const burgerRef = useRef(null);
+  const [isOpen, setOpen] = useState(false);
+  const toggleBurger = () => {
+    setOpen(!isOpen);
+  };
+  /*
+  useEffect(() => {
+    const openBurger = () => {
+      burgerRef.current.classList.add
+    };
+    if (burgerRef.current) {
+      burgerRef.current.addEventListener("click", openBurger);
+    }
+  }, []);
+  */
   return (
-    <div className="cursor-pointer flex justify-center items-center w-14 h-14 xl:hidden py-4 rounded-xl bg-neutral-black-elbrus/20 backdrop-blur-xs">
-      <div id="burger-div" className="flex flex-col gap-[4.5px] my-1 mx-1.5">
-        <span id="burger-line-top" className="block w-4 h-px bg-neutral-bright-beginning"></span>
-        <span id="burger-line-middle" className="block w-4 h-px bg-neutral-bright-beginning"></span>
-        <span id="burger-line-bottom" className="block w-4 h-px bg-neutral-bright-beginning"></span>
+    <div /* ref ={burgerRef} */ className={`flex flex-col justify-start items-start w-14 h-14 xl:hidden py-4 bg-neutral-black-elbrus/20 backdrop-blur-xs transition-250ms overflow-clip ${isOpen ? "fixed top-0 right-0 w-93.75 h-svh rounded-none" : "absolute top-0 right-0 rounded-xl"}`}>
+      <div className={`relative ${isOpen ? "px-12 py-8" : "px-0 py-0"} xl:px-8 w-full flex flex-row ${isOpen ? "justify-end items-center" : "justify-center items-center"}`}>
+        <Image
+          className={`absolute top-5 left-10 opacity-0 transition-default ${isOpen ? "opacity-100" : ""}`}
+          src={Logo}
+          alt="ИННОВАТИКА"
+          width="24"
+          height="32"
+        />
+        <div onClick={toggleBurger} id="burger-div" className={`cursor-pointer flex flex-col gap-[4.5px] ${isOpen ? "my-1.5 mx-1.5" : "my-1.5 mx-1.5"}`}>
+          <span id="burger-line-top" className={`${isOpen ? "rotate-45" : ""} transition-default block w-4 h-px bg-neutral-bright-beginning`}></span>
+          <span id="burger-line-middle" className={`${isOpen ? "opacity-0" : ""} transition-default block w-4 h-px bg-neutral-bright-beginning`}></span>
+          <span id="burger-line-bottom" className={`${isOpen ? "-rotate-45 translate-xy-half" : ""} transition-default block w-4 h-px bg-neutral-bright-beginning`}></span>
+        </div>
       </div>
+      <nav className="w-93.75 h-100svh flex flex-col text-neutral-bright-beginning text-[14px] font-normal">
+        <ul className="flex flex-col text-neutral-bright-beginning text-[14px] font-nav font-normal">
+          <li className="px-12 py-4"><a href="#!" className="uppercase">ПРОДУКТЫ</a></li>
+          <li className="px-12 py-4"><a href="#!" className="uppercase">О КОМПАНИИ</a></li>
+          <li className="px-12 py-4"><a href="#!" className="uppercase">ЭКСПЕРТИЗА</a></li>
+          <li className="px-12 py-4"><a href="#!" className="uppercase">КАРЬЕРА</a></li>
+          <li className="px-12 py-4"><a href="#!" className="uppercase">ИНВЕСТОРАМ</a></li>
+          <li className="px-12 py-4"><a href="#!" className="uppercase">ENGLISH</a></li>
+        </ul>
+      </nav>
+      <button className="cursor-pointer absolute bottom-16 mx-5 py-4.5 w-full bg-neutral-black-elbrus/20 rounded-2xl backdrop-blur-xs font-nav text-[16px] uppercase">
+        СВЯЗАТЬСЯ
+      </button>
     </div>
   );
 };
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,10 +6,10 @@ import { Burger } from "./Burger";
 
 export const Header = () => {
   return (
-    <header className="w-full md:w-180 xl:w-auto fixed top-0 my-8 px-6 z-20 text-white">
-      <div className="w-full flex flex-row justify-between">
+    <header className="w-full md:w-180 xl:w-auto fixed top-0 my-8 px-6 z-20 text-neutral-bright-beginning">
+      <div className="relative w-full flex flex-row justify-between">
         <div className="flex justify-between items-center rounded-xl bg-neutral-black-elbrus/20 backdrop-blur-xs">
-          <div className="px-4 xl:px-8">
+          <div className="px-4 py-3 xl:px-8 xl:py-0">
             <Image
               className=""
               src={Logo}
