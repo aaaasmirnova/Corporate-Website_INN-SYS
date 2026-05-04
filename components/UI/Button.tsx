4652434233db4f732d1841b40c0f8c8f90ddefd7
aaaasmirnova <smirnovnana@yandex.ru>
2026-05-05 00:04:33 +0300

    feat: add prop

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -1,11 +1,15 @@
 interface ButtonProps {
   children: React.ReactNode;
   customClassName: string;
+  onClick?: () => void;
 }
 
-export const Button = ({ children, customClassName }: ButtonProps) => {
+export const Button = ({ children, customClassName, onClick }: ButtonProps) => {
   return (
-    <button className={`${customClassName} rounded-[16px] py-3`}>
+    <button
+      className={`${customClassName} rounded-[16px] py-3`}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
