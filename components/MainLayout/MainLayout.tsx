b39240370d4cbfa37f7c20b9d6581afe3114b718
aaaasmirnova <smirnovnana@yandex.ru>
2026-05-05 00:04:15 +0300

    style: delete padding

--- components/MainLayout/MainLayout.tsx
+++ components/MainLayout/MainLayout.tsx
@@ -7,7 +7,7 @@ interface MainLayoutProps {
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className="min-h-screen bg-neutral-black-elbrus">
-      <div className="px-6 md:px-8 xl:px-24">{children}</div>
+      <div>{children}</div>
     </div>
   );
 };
