79dd5236e7d0563bf82b1d6ad04154686691e42b
Mariya Shusharina <mariya.shushar@gmail.com>
2026-05-04 19:17:31 +0300

    fix: correct max-width of KeyCards and transform-translate values

--- components/KeyIndicatorsComponent/KeyCard.tsx
+++ components/KeyIndicatorsComponent/KeyCard.tsx
@@ -4,7 +4,7 @@ interface IKeyCardProps {
 }
 
 export const KeyCard = ({ info }: IKeyCardProps) => {
-  const cardClass: string = "p-6 max-w-[400px] flex flex-col gap-2 bg-neutral-bright-beginning-4 backdrop-blur-xs rounded-3xl " +
+  const cardClass: string = "p-6 max-w-82 md:max-w-100 flex flex-col gap-2 bg-neutral-bright-beginning-4 backdrop-blur-xs rounded-3xl " +
   `card-${info.id}`;
 
   return (
