ef1ef0822abe3debf51641cedd18f4596eb8761d
Mariya Shusharina <mariya.shushar@gmail.com>
2026-05-07 16:21:50 +0300

    fix: add w-full to layout containers for correcting background of HeroSection

--- components/MainLayout/MainLayout.tsx
+++ components/MainLayout/MainLayout.tsx
@@ -6,8 +6,8 @@ interface MainLayoutProps {
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="min-h-screen flex flex-col items-center bg-neutral-black-elbrus">
-      <div className="min-h-screen flex flex-col items-center relative">{children}</div>
+    <div className="w-full min-h-screen flex flex-col items-center bg-neutral-black-elbrus">
+      <div className="w-full min-h-screen flex flex-col items-center relative">{children}</div>
     </div>
   );
 };
