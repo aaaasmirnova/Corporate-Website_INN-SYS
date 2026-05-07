41674af92c441ea2f6d103dbce1178519fbd5351
Mariya Shusharina <mariya.shushar@gmail.com>
2026-05-07 16:41:55 +0300

    fix: correct width of Header on tablet

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -6,7 +6,7 @@ import { Burger } from "./Burger";
 
 export const Header = () => {
   return (
-    <header className="w-full md:w-180 xl:w-auto fixed top-0 my-8 px-6 z-20 text-neutral-bright-beginning">
+    <header className="w-full  xl:w-auto fixed top-0 my-8 px-6 z-20 text-neutral-bright-beginning">
       <div className="relative w-full flex flex-row justify-between">
         <div className="flex justify-between items-center rounded-xl bg-neutral-black-elbrus/20 backdrop-blur-xs">
           <div className="px-4 py-3 xl:px-8 xl:py-0">
