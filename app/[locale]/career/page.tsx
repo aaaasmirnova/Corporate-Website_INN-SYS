c95149a1f80e603d3fd5954a379a84af29115f95
aaaasmirnova <smirnovnana@yandex.ru>
2026-05-12 18:15:29 +0300

    fix

--- app/[locale]/career/page.tsx
+++ app/[locale]/career/page.tsx
@@ -1,7 +1,5 @@
-import { Footer } from "@/components/common/Footer";
 import { Career } from "@/components/career/Career";
 import { MainLayout } from "@/components/MainLayout/MainLayout";
-import ContactsForm from "@/app/components/ContactsForm/ContactsForm";
 import CareerForm from "@/app/components/CareerForm/CareerForm";
 
 export default function CareerPage() {
@@ -9,7 +7,6 @@ export default function CareerPage() {
     <MainLayout>
       <Career />
       <CareerForm />
-      {/* <Footer /> */}
     </MainLayout>
   );
 }
--- app/[locale]/contacts/page.tsx
+++ app/[locale]/contacts/page.tsx
@@ -1,6 +1,4 @@
 import { MainLayout } from "@/components/MainLayout/MainLayout";
-
-import CareerForm from "@/app/components/CareerForm/CareerForm";
 import { ContactsHero } from "@/components/contacts/ContactsHero";
 import ContactsForm from "@/app/components/ContactsForm/ContactsForm";
 
