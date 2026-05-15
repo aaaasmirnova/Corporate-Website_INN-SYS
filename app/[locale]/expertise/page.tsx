985a8105dfc105e774e164f7309a02105aad490c
Suren Simonyan <s.simonyan@combotech.net>
2026-05-15 23:44:09 +0400

    feat: added expertise and tabs section

--- app/[locale]/expertise/page.tsx
+++ app/[locale]/expertise/page.tsx
@@ -1,12 +1,14 @@
 import { Expertise } from "@/components/Expertise/Expertise";
 import { Header } from "@/components/common/Header/Header";
 import { MainLayout } from "@/components/MainLayout/MainLayout";
+import { ExpertiseTabs } from "@/app/components/ExpertiseTabs/ExpertiseTabs";
 
-export default function ProductsPage() {
+export default function ExpertisePage() {
   return (
     <MainLayout>
       <Header hasButton={true} />
       <Expertise />
+      <ExpertiseTabs />
     </MainLayout>
   )
 }
