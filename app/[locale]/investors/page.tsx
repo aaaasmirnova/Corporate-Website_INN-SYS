31c7c5dced4ec376f3122d5f616c07908bfd391a
Mariya Shusharina <mariya.shushar@gmail.com>
2026-05-18 23:48:28 +0300

    fix: remove draft Contacts and Investors files, correct back imports for Investors section

--- app/[locale]/investors/page.tsx
+++ app/[locale]/investors/page.tsx
@@ -1,17 +1,15 @@
-// import { Investors } from "@/components/Investors/Investors";
 import { Header } from "@/components/common/Header/Header";
 import { MainLayout } from "@/components/MainLayout/MainLayout";
 import ContactsForm from "@/app/components/ContactsForm/ContactsForm";
-import { InvestorsHero } from "@/components/Investors/InvestorsHero";
-import { AnnualReport } from "@/components/Investors/AnnualReport";
-import { CompanyRule } from "@/components/Investors/CompanyRule";
+import { InvestorsHero } from "@/components/investors/InvestorsHero";
+import { AnnualReport } from "@/components/investors/AnnualReport";
+import { CompanyRule } from "@/components/investors/CompanyRule";
 
 export default function ProductsPage() {
   return (
     <MainLayout>
       <Header hasButton={true} />
       <InvestorsHero />
-      {/* <Investors /> */}
       <AnnualReport />
       <CompanyRule />
       <ContactsForm />
